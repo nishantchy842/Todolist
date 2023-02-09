add00fccf191d8410947e8bcc7438ccdcb4c86fc
Nishant chaudhary <nishantchaudhary842@gmail.com>
2023-02-09 16:19:15 +0545

    database connected

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,20 @@
+import { error } from "console";
 import mongoose from "mongoose";
+import { connected, disconnect } from "process";
 
 const Connection = () =>{
 
     const mongo_url = 'mongodb+srv://nishant:12345@merntodo.cb3mmyq.mongodb.net/?retryWrites=true&w=majority'
-    mongoose.connect(mongo_url,{useNewUrlParse:true})
+    mongoose.connect(mongo_url,{useNewUrlParser:true})
+
+    mongoose.connection.on('connected',()=>{
+        console.log("database is successfully connected")
+    })
+    mongoose.connection.on('disconnected',()=>{
+        console.log("database is not connected")
+    })
+    mongoose.connection.on('error',()=>{
+        console.log("database has something error",error.message)
+    })
 }
+export default Connection
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import Connection from './database/db.js'
  const app = express()
  const port = 8000
+ Connection()
  app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
